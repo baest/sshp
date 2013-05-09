8b4b13ef2f499c9c7da94224e159f630cc5a4c9b
Martin Kjeldsen <martin@martinkjeldsen.dk>
2013-05-09 20:14:38 +0200

    Support recursive defines and local_defines

--- sshp.pl
+++ sshp.pl
@@ -6,58 +6,83 @@ use Data::Dumper;
 
 my @ssh_file = read_file("$ENV{HOME}/.ssh/config");
 
+my @define_stack = ();
 my %defines = ();
 my @output = ();
 
 #support defines with params
 #define xx(x, y) xx $x $y
 
+#STOP (stop processing)
+
+#local define (for port number or IP for block) (howto define block?)
+
+#MISSING
 #support templates (multiline defines with params) (start and stop?)
 
 #conditional defines (should be depending on arguments to sshp like @home)
 
-#STOP (stop processing)
+#define support as argument
 
 foreach (@ssh_file) {
 	chomp;
 
-	if (/^\#define\s+(\w+)\(([^\)]+)\)\s+(.*)/) {
+	$_ = replace_define($_);
+	my $push = 0;
+
+	if (/^\#(?<local>local_)?define\s+(?<name>\w+)(?:\((?<args>[^\)]+)\)\s+)?(?<template>.*)/) {
 		my $i = 0;
 		my %args = ();
 		my @defaults;
 
-		foreach (split(/\s*,\s*/, $2)) {
-			my ($arg, $default) = split(':', $_, 2);
-			$args{$arg} = $i++;
-			push @defaults, $default;
+		if ($+{local}) {
+			push @define_stack, {%defines};
 		}
 
-		$defines{$1} = { template => $3, params => \%args, defaults => \@defaults };
-	}
-	elsif (/^\#define\s+(\w+)\s+(.*)/) {
-		$defines{$1} = { template => $3 };
-	}
-	elsif (/\#(\w+)(?:\(([^\)]+)\))?/ && exists $defines{$1}) {
-		my $define = $defines{$1};
-		my $data = $define->{template};
+		if ($+{args}) {
+			foreach (split(/\s*,\s*/, $+{args})) {
+				my ($arg, $default) = split(':', $_, 2);
+				$args{$arg} = $i++;
+				push @defaults, $default;
+			}
 
-		if (exists $define->{params}) {
-			my $params = $define->{params};
-			my $defaults = $define->{defaults};
-			my @args = split(/\s*,\s*/, $2);
-
-			my $regex = '\\$(' . join('|', keys %$params) . ')';
-			$data =~ s!$regex!$args[$params->{$1}] || $defaults->[$params->{$1}]!e;
+			$defines{$+{name}} = { template => $+{template}, params => \%args, defaults => \@defaults };
 		}
-
-		push @output, $data;
+		else {
+			$defines{$+{name}} = { template => $+{template} };
+		}
 	}
 	elsif (/^\#STOP/) {
 		last;
 	}
-	else {
-		push @output, $_;
+	elsif (/^\s*$/) { #block end
+		%defines = %{pop @define_stack} if @define_stack;
+		$push = 1;
 	}
+	else {
+		$push = 1;
+	}
+	push @output, $_ if $push;
+}
+
+sub replace_define {
+	my ($line) = @_;
+
+	return $line unless $line =~ /\#(\w+)(?:\(([^\)]+)\))?/ && exists $defines{$1};
+
+	my $define = $defines{$1};
+	my $data = $define->{template};
+
+	if (exists $define->{params}) {
+		my $params = $define->{params};
+		my $defaults = $define->{defaults};
+		my @args = map { replace_define($_) } split(/\s*,\s*/, $2);
+
+		my $regex = '\\$(' . join('|', keys %$params) . ')';
+		$data =~ s!$regex!$args[$params->{$1}] || $defaults->[$params->{$1}]!e;
+	}
+
+	return $data;
 }
 
 #warn Dumper(\%defines);
